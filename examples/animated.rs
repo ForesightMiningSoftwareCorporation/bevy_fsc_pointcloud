48eb11f5ea458ba18f9463745172743f5e766a96
TheRawMeatball <therawmeatball@gmail.com>
2023-08-21 12:28:00 +0300

    switch to bevy_flycam for the examples

--- examples/animated.rs
+++ examples/animated.rs
@@ -1,15 +1,11 @@
 use bevy::prelude::*;
+use bevy_flycam::PlayerPlugin;
 use bevy_fsc_point_cloud::{PointCloudAsset, PointCloudPlaybackControl, PotreePointCloud};
-use smooth_bevy_cameras::{
-    controllers::fps::{FpsCameraBundle, FpsCameraPlugin},
-    LookTransformPlugin,
-};
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins.set(WindowPlugin::default()))
-        .add_plugin(LookTransformPlugin)
-        .add_plugin(FpsCameraPlugin::default())
+        .add_plugin(PlayerPlugin)
         .add_plugin(bevy_fsc_point_cloud::PointCloudPlugin {
             colored: false,
             animated: true,
@@ -31,16 +27,4 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
             point_size: 3.0,
         })
         .insert(SpatialBundle::default());
-
-    commands
-        .spawn(Camera3dBundle {
-            transform: Transform::from_xyz(1.0, 1.5, 1.0).looking_at(Vec3::ZERO, Vec3::Y),
-            ..default()
-        })
-        .insert(FpsCameraBundle::new(
-            Default::default(),
-            Vec3::new(30.0, 30.0, 30.0),
-            Vec3::ZERO,
-            Vec3::Y
-        ));
 }
